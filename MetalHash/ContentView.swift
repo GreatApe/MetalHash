f147934af9a864a1f8dae59d63566a77ffab5f68
Gustaf Kugelberg <kugel@kth.se>
2023-08-09 22:11:42 +0200

    Initial Commit

---
+++ MetalHash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetalHash
+//
+//  Created by Gustaf Kugelberg on 09/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MetalHash/MetalHashApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalHashApp.swift
+//  MetalHash
+//
+//  Created by Gustaf Kugelberg on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MetalHashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
